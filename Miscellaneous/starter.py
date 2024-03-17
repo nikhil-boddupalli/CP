fc2c302629cf55267db03cf5a88bee31186790a0
nikhil-boddupalli <50843611+nikhil-boddupalli@users.noreply.github.com>
2024-03-17 17:40:56 +0530

    Create starter.py

---
+++ Miscellaneous/starter.py
@@ -0,0 +1,17 @@
+def ip():
+    return input()
+
+def ip_int():
+    return int(input())
+
+def ip_arr():
+    return list(input().split())
+
+def ip_int_arr():
+    return list(map(int, input().split()))
+
+def ip_int_tuple():
+    return map(int, input().split())
+
+def ip_tuple():
+    return input().split()
